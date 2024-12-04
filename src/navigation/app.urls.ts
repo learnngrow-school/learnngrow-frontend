ed340bd8b17ef515a22bfad68d203bb953e8503c
MikhailZhuravlevUrFU <zhuravlevmikhail@urfu.ru>
2024-12-04 15:11:02 +0500

    add shop; add course detail; add filters

--- src/navigation/app.urls.ts
+++ src/navigation/app.urls.ts
@@ -14,4 +14,5 @@ export const urls = {
   pupils: '/me/pupils',
   shop: '/me/shop',
   myCourses: '/me/courses',
+  myCoursesDeatil: '/me/courses',
 }
---
+++ src/screens/Shop/index.tsx
@@ -0,0 +1,95 @@
+import { FC, useRef, useState } from "react";
+import CourseCard from "../Courses/components/CourseCard/course-card";
+import { useNavigate } from "react-router-dom";
+import "./shop.css";
+import { useCourses } from "../../services/courses.service";
+import SearchInput from "../../shared/Inputs/SearchInput";
+import BaseButton from "../../shared/Buttons/BaseButton";
+import FilterDropdown from "../Courses/components/FilterDropdown/filter-dropdown";
+import Search from "../../assets/icons/search.svg";
+import { urls } from "../../navigation/app.urls";
+
+const Shop: FC = () => {
+    const { courses } = useCourses();
+    const [isFilterOpen, setIsFilterOpen] = useState(false);
+    const [filters, setFilters] = useState<{ [key: string]: boolean }>({});
+    const filterButtonRef = useRef<HTMLButtonElement>(null);
+    const pageRef = useRef<HTMLDivElement>(null);
+    const navigate = useNavigate();
+
+    const handleFilterChange = (newFilters: { [key: string]: boolean }) => {
+        setFilters(newFilters);
+    };
+
+    const handleCourseClick = (courseId: string) => {
+        navigate(`${urls.myCoursesDeatil}/${courseId}`);
+    };
+
+    const filteredCourses = courses.filter((courseData) => {
+        const selectedSubjects = [];
+        if (filters.math) selectedSubjects.push("Математика");
+        if (filters.informatics) selectedSubjects.push("Информатика");
+        if (filters.russianlang) selectedSubjects.push("Русский язык");
+
+        const categoryMatch =
+            selectedSubjects.length === 0 ||
+            selectedSubjects.includes(courseData.subject?.title);
+    
+        return categoryMatch;
+    });
+
+    return (
+        <div className="courses-page" ref={pageRef}>
+            <section className="sections">
+                <section className="courses-section">
+                    <h2 className="section-title section-title--green">Магазин</h2>
+
+                    <div className="search-and-filter-container-shop">
+                        <div className="search-container-shop">
+                            <SearchInput />
+                            <img src={Search} alt="search" className="search-icon" />
+                        </div>
+
+                        <BaseButton
+                            text="Фильтр"
+                            theme="white-secondary"
+                            className="btn-filter-shop"
+                            onClick={() => setIsFilterOpen(!isFilterOpen)}
+                            ref={filterButtonRef}
+                        />
+                        <FilterDropdown
+                            isOpen={isFilterOpen}
+                            buttonRef={filterButtonRef}
+                            pageRef={pageRef}
+                            onClose={() => setIsFilterOpen(false)}
+                            onFilterChange={handleFilterChange}
+                        />
+                    </div>
+
+                    <div className="course-list">
+                        {filteredCourses.map((courseData) => {
+                            const { title, price, id } = courseData.course || {};
+                            const subject = courseData.subject?.title || "Предмет не указан";
+                            return (
+                                <div
+                                    key={id}
+                                    onClick={() => handleCourseClick(id)}
+                                    style={{ cursor: "pointer" }}
+                                >
+                                    <CourseCard
+                                        key={id}
+                                        title={title || "Не указано"}
+                                        subject={subject}
+                                        price={price != null ? price.toString() : "0"}
+                                    />
+                                </div>
+                            );
+                        })}
+                    </div>
+                </section>
+            </section>
+        </div>
+    );
+};
+
+export default Shop;
